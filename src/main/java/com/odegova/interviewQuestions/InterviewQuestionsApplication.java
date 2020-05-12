4a7fdaf01581744c8564e90aefedfdc90b402aa3
Oxana Odegova <oxyapple@yandex.ru>
2020-05-12 11:35:57 +0200

    new project

---
+++ src/main/java/com/odegova/interviewQuestions/InterviewQuestionsApplication.java
@@ -0,0 +1,13 @@
+package com.odegova.interviewQuestions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewQuestionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterviewQuestionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/odegova/interviewQuestions/InterviewQuestionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.odegova.interviewQuestions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewQuestionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
